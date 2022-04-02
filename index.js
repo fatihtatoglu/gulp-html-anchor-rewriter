8f7bf0b48953f3b196374a8e15336570c2560e79
Fatih Tatoğlu <fatih@tatoglu.net>
2022-04-02 14:54:44 +0300

    complete Cognitive Complexity refactoring

--- index.js
+++ index.js
@@ -8,33 +8,60 @@ const PLUGIN_NAME = "gulp-html-anchor-rewriter";
 
 class Plugin {
 
+    /**
+    * The plugin config object.
+    * @typedef {Object} PluginConfig
+    * @property {string | Array.<string>} keyword For searching the anchor element href attribute value. If the href contains the keyword, the anchor will be processed.
+    * @property {string} rel For adding rel attribute.
+    * @property {string} target For adding target attribute.
+    * @property {boolean} whiteList Indicates whether the white-list mode is enabled.
+    */
+
+    /**
+     * @type {PluginConfig}
+     */
     #options;
 
+    /**
+     * Creates a new instance of the plugin.
+     * @param {PluginConfig} options The configuration object.
+     */
     constructor(options) {
-        this.#validateOption(options);
         this.#options = options;
     }
 
+    /**
+     * @returns {string}
+     */
     get #keyword() {
         return this.#options["keyword"];
     }
 
+    /**
+     * @returns {string}
+     */
     get #rel() {
         return this.#options["rel"];
     }
 
+    /**
+     * @returns {string}
+     */
     get #target() {
         return this.#options["target"];
     }
 
+    /**
+     * @returns {boolean}
+     */
     get #whiteList() {
         return this.#options["whiteList"];
     }
 
     /**
      * Runs the plugin logic.
-     * @param {String} content The raw content of the file.
-     * @returns {String}
+     * @param {string} content The raw content of the file.
+     * @returns {string}
      */
     run(content) {
         let that = this;
@@ -48,50 +75,75 @@ class Plugin {
         return html.toString();
     }
 
-    #validateOption(options) {
-        if (!options) {
-            throw new PluginError(PLUGIN_NAME, "The options are missing!");
+    /**
+     * Validates the configuration options.
+     * @returns {PluginError}
+     */
+    validate() {
+        if (!this.#options) {
+            return new PluginError(PLUGIN_NAME, "The options are missing!");
         }
 
-        if (options["keyword"]) {
-            var isValid = Array.isArray(options["keyword"]) || typeof options["keyword"] === "string";
+        if (this.#keyword) {
+            let isValid = Array.isArray(this.#keyword) || typeof this.#keyword === "string";
             if (!isValid) {
-                throw new PluginError(PLUGIN_NAME, "The keyword must be string or array of the strings!");
+                return new PluginError(PLUGIN_NAME, "The keyword must be string or array of the strings!");
             }
         }
     }
 
-    #processAnchor(element, keyword, rel, target, whiteList) {
+    /**
+     * Processes the rules for an anchor element.
+     * @param {htmlParser.HTMLElement} anchorElement The anchor element.
+     * @param {string | Array.<string>} keyword The keyword.
+     * @param {string} rel The rel attribute.
+     * @param {string} target The target attribute.
+     * @param {boolean} whiteList Indicates whether the white-list mode is enabled.
+     */
+    #processAnchor(anchorElement, keyword, rel, target, whiteList) {
 
         if (!keyword) {
-            this.#setAttribute(element, rel, target);
+            this.#setAttribute(anchorElement, rel, target);
             return;
         }
 
-        var keywords = keyword;
+        let keywords = keyword;
         if (typeof keyword === "string") {
             keywords = [keyword];
         }
 
-        if (!this.#canExecute(element, keywords, whiteList)) {
+        if (!this.#canExecute(anchorElement, keywords, whiteList)) {
             return;
         }
 
-        this.#setAttribute(element, rel, target);
+        this.#setAttribute(anchorElement, rel, target);
     }
 
-    #setAttribute(element, rel, target) {
-        if (rel && !element.getAttribute("rel")) {
-            element.setAttribute("rel", rel);
+    /**
+     * Sets the anchor attributes.
+     * @param {htmlParser.HTMLElement} anchorElement The anchor element.
+     * @param {string} rel The rel attribute.
+     * @param {string} target The target attribute. 
+     */
+    #setAttribute(anchorElement, rel, target) {
+        if (rel && !anchorElement.getAttribute("rel")) {
+            anchorElement.setAttribute("rel", rel);
         }
 
-        if (target && !element.getAttribute("target")) {
-            element.setAttribute("target", target);
+        if (target && !anchorElement.getAttribute("target")) {
+            anchorElement.setAttribute("target", target);
         }
     }
 
-    #canExecute(element, keywords, whiteList) {
-        var href = element.getAttribute("href");
+    /**
+     * Determines an anchor element will be re-write or not.
+     * @param {htmlParser.HTMLElement} anchorElement The anchor element.
+     * @param {string | Array.<string>} keywords The array of the keywords.
+     * @param {boolean} whiteList Indicates whether the white-list mode is enabled.
+     * @returns {boolean}
+     */
+    #canExecute(anchorElement, keywords, whiteList) {
+        var href = anchorElement.getAttribute("href");
 
         var result = keywords.some(function (k) {
             return href.indexOf(k) >= 0;
@@ -111,6 +163,12 @@ function pluginFunction(options) {
 
     return through.obj(function (file, _encoding, cb) {
 
+        let validationResult = plugin.validate();
+        if (validationResult) {
+            cb(validationResult, file);
+            return;
+        }
+
         // ignore empty files.
         if (file.isNull()) {
             cb();
@@ -123,8 +181,8 @@ function pluginFunction(options) {
             return;
         }
 
-        let rawContent = file.contents.toString();
-        let pluginOutput = plugin.run(rawContent);
+        let content = file.contents.toString();
+        let pluginOutput = plugin.run(content);
 
         file.contents = Buffer.from(pluginOutput);
 
